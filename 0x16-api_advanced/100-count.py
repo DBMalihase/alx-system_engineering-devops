fae124681d1f61a0c8755ddf9dc3b1b85a099de1
DBMalihase <111195619+DBMalihase@users.noreply.github.com>
2023-08-10 18:46:20 +0200

    Api advanced

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,45 +1,90 @@
 #!/usr/bin/python3
-# get subs
-from requests import get
-from sys import argv
-
-hotlist = []
-after = None
-
-
-def count_all(hotlist, word_list):
-    count_dic = {word.lower(): 0 for word in word_list}
-    for title in hotlist:
-        words = title.split(' ')
-        for word in words:
-            if count_dic.get(word) is not None:
-                count_dic[word] += 1
-
-    for key in sorted(count_dic, key=count_dic.get, reverse=True):
-        if count_dic.get(key):
-            for thing in word_list:
-                if key == thing.lower():
-                    print("{}: {}".format(thing, count_dic[key]))
+"""script for parsing web data from an api
+    DISCLAIMER: THIS PROBABLY SHOULDN'T BE DONE RECURSIVELY
+    but we had to for school :P
+"""
+import json
+import requests
+import sys
 
 
-def count_words(subreddit, word_list):
-    global hotlist
-    global after
-    """subs"""
-    head = {'User-Agent': 'Dan Kazam'}
-    if after:
-        count = get('https://www.reddit.com/r/{}/hot.json?after={}'.format(
-            subreddit, after), headers=head).json().get('data')
+def get_hot_posts(subreddit, hot_list=[]):
+    """api call to reddit to get the number of subscribers
+    """
+    base_url = 'https://www.reddit.com/r/{}/top.json'.format(
+        subreddit
+    )
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    if len(hot_list) == 0:
+        # grab initial list
+        url = base_url
     else:
-        count = get('https://www.reddit.com/r/{}/hot.json'.format(
-            subreddit), headers=head).json().get('data')
-    hotlist += [dic.get('data').get('title').lower()
-                for dic in count.get('children')]
-    after = count.get('after')
-    if after:
-        return count_words(subreddit, word_list)
-    return count_all(hotlist, word_list)
+        # grab next pagination after last obj in hot_list
+        url = base_url + '?after={}_{}'.format(
+            hot_list[-1].get('kind'),
+            hot_list[-1].get('data').get('id')
+        )
+    response = requests.get(url, headers=headers)
+    resp = json.loads(response.text)
+    try:
+        data = resp.get('data')
+        children = data.get('children')
+    except:
+        return None
+    if children is None or data is None or len(children) < 1:
+        return hot_list
+    hot_list.extend(children)
+    return get_hot_posts(subreddit, hot_list)
 
 
-if __name__ == "__main__":
-    count_words(argv[1], argv[2].split(' '))
+def count_words(subreddit, wordlist):
+    """count words in titles of hot posts for subreddit
+    """
+    posts = get_hot_posts(subreddit)
+    if posts is None:
+        print(end="")
+        return
+    words = gather_word_info(posts, wordlist)
+    sorted_list = [(key, val) for key, val in words.items()]
+    sorted_list = sorted(sorted_list, key=lambda tup: tup[1], reverse=True)
+    [print("{}: {}".format(key, val)) for (key, val) in sorted_list if val > 0]
+
+
+def gather_word_info(hot_posts, wordlist,
+                     posts_len=None,
+                     counter=0,
+                     words_info=None):
+    """does the recursion to grab word info from wordlist and posts
+    """
+    if hot_posts is None:
+        return
+    # generate defaults
+    if posts_len is None:
+        posts_len = len(hot_posts)
+    if words_info is None:
+        words_info = {key: 0 for key in wordlist}
+    # base case
+    if counter == posts_len - 1:
+        return words_info
+
+    # parse this title and move to next
+    data = hot_posts[counter].get('data')
+    if data is None:
+        return words_info
+    title = data.get('title')
+    if title is None:
+        return words_info
+    # im sorry im not doing recursion for text parsing that's rediculous
+    for word in title.split(' '):
+        word = word.lower()
+        if word in wordlist:
+            words_info[word] += 1
+    counter += 1
+    return gather_word_info(
+        hot_posts, wordlist, posts_len,
+        counter, words_info
+    )
